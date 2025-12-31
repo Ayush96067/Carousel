cd34bd3df1c29827cb72b6bcb482e250c9dcba48
Ayush <ayushsharma722001@gmail.com>
2025-12-31 18:12:18 +0530

    Display slide on hover on navLinks

--- src/components/FigmaComponents/Navbar.jsx
+++ src/components/FigmaComponents/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { CiHeart, CiSearch, CiUser, CiMenuBurger } from "react-icons/ci";
 import { MdOutlineShoppingBag, MdClose } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import SlideBar from "./FigmaUI/SlideBar";
 import styles from "../../Styles/FigmaStyles/Navbar.module.css";
+import { MENU_DATA } from "../../lib/figmaData";
 
 const NAV_LINKS = [
   "Rings",
@@ -14,18 +15,27 @@ const NAV_LINKS = [
   "Bracelets",
   "Collections",
   "Gifts",
-  "The edit",
+  "Edit",
 ];
 
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(null);
 
   // Helper to close menu when a link is clicked
   const handleClick = () => setIsMobileMenuOpen((open) => !open);
 
+  // console.log();
+
   return (
     <>
-      <header className={styles.navContainer}>
+      <div
+        className={`fixed inset-0 bg-black/10 backdrop-blur-sm z-40 ${activeMenu ? "opacity-100 visible" : "opacity-0 invisible pointer-events-none"}`}
+      />
+      <header
+        className={styles.navContainer}
+        onMouseLeave={() => setActiveMenu(null)}
+      >
         {/* TOP ROW */}
         <div className={styles.navUpperBar}>
           <div className={styles.navContactInfo}>
@@ -67,12 +77,23 @@ function Navbar() {
         <nav className="hidden md:flex justify-center py-4 px-4">
           <ul className="flex gap-8 lg:gap-12 whitespace-nowrap">
             {NAV_LINKS.map((type) => (
-              <NavLink key={type} href={`#${type}`}>
+              <NavLink
+                href={`#${type}`}
+                className={`${styles.nav_link} hover:text-black hover:underline `}
+                onMouseEnter={() => setActiveMenu(type)}
+              >
                 {type}
               </NavLink>
             ))}
           </ul>
         </nav>
+        <div
+          className={`absolute left-0 w-full top-full bg-white border-t border-gray-100 shadow-xl overflow-hidden origin-top ${activeMenu ? "max-h-140 opacity-100 visible" : "max-h-0 opacity-0 invisible"} `}
+        >
+          {activeMenu && MENU_DATA[activeMenu] && (
+            <MenuBar menu={MENU_DATA[activeMenu]} />
+          )}
+        </div>
       </header>
 
       {/* --- MOBILE DRAWER & BACKDROP --- */}
@@ -129,15 +150,88 @@ const IconButton = ({ icon, label }) => (
   </button>
 );
 
-const NavLink = ({ href, children }) => (
-  <li>
-    <a
-      href={href}
-      className={`${styles.nav_link} hover:text-black hover:underline `}
-    >
-      {children}
-    </a>
-  </li>
-);
+function MenuBar({ menu }) {
+  if (!menu) return null;
+
+  return (
+    <div className="w-full flex justify-center  bg-white border-t border-gray-100">
+      <div className="w-full mx-auto px-10  py-10  xl:columns-5 gap-x-10 gap-y-10 space-y-52">
+        {menu.map((item, index) => (
+          <div
+            key={index}
+            className="break-inside-avoid self-center inline-block w-full mb-10"
+          >
+            <h3 className="font-serif font-bold text-sm uppercase tracking-wider mb-3 text-gray-900 border-b border-gray-100 pb-2">
+              {item.title}
+            </h3>
+
+            <ul className="flex flex-col gap-5">
+              {item.list.map((listItem, i) => (
+                <li
+                  key={i}
+                  className="text-sm text-gray-500 hover:text-black hover:underline underline-offset-4 cursor-pointer transition-colors"
+                >
+                  {listItem}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <div
+          key={index}
+          className="break-inside-avoid self-center inline-block w-full mb-10"
+        >
+          <h3 className="font-serif font-bold text-sm uppercase tracking-wider mb-3 text-gray-900 border-b border-gray-100 pb-2">
+            {item.title}
+          </h3>
+
+          <ul className="flex flex-col gap-5">
+            {item.list.map((listItem, i) => (
+              <li
+                key={i}
+                className="text-sm text-gray-500 hover:text-black hover:underline underline-offset-4 cursor-pointer transition-colors"
+              >
+                {listItem}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-60 border h-60">
+          <img src="./FigmaImage/img1.png" alt="Ring" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// function MenuBar({ menu }) {
+//   if (!menu) return null;
+
+//   return (
+//     <div className="w-full px-10 py-8 bg-white columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-8">
+//       {menu.map((item, index) => (
+//         <div
+//           key={index}
+//           className="break-inside-avoid mb-8 inline-block w-full"
+//         >
+//           <h3 className="font-serif font-bold text-sm uppercase tracking-wider mb-3 text-gray-900 border-b border-gray-100 pb-1">
+//             {item.title}
+//           </h3>
+
+//           <ul className="flex flex-col gap-2">
+//             {item.list.map((listItem, i) => (
+//               <li
+//                 key={i}
+//                 className="text-sm text-gray-500 hover:text-black hover:underline cursor-pointer transition-colors"
+//               >
+//                 {listItem}
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 export default Navbar;
--- src/lib/figmaData.js
+++ src/lib/figmaData.js
@@ -329,3 +329,670 @@ export const COLOR_OPTIONS = [
   { label: "White", color: "bg-gray-200" },
   { label: "Yellow", color: "bg-yellow-200" },
 ];
+
+export const MENU_DATA = {
+  Rings: [
+    {
+      title: "Natural Gemstone Rings",
+      list: [
+        "Blue Sapphire Rings",
+        "Ruby Rings",
+        "Emerald Rings",
+        "Opal Rings",
+        "Aquamarine Rings",
+        "Amethyst Rings",
+        "Garnet Rings",
+        "London Blue Topaz Rings",
+        "Morganite Rings",
+        "Tanzanite Rings",
+        "Citrine Rings",
+      ],
+    },
+    {
+      title: "Natural Diamond Rings",
+      list: ["Diamond Rings", "Colored Diamond Rings"],
+    },
+    {
+      title: "Lab-Grown Rings",
+      list: [
+        "Lab Diamond Rings",
+        "Lab Colored Diamond Rings",
+        "Lab Emerald Rings",
+        "Lab Blue Sapphire Rings",
+        "Lab Ruby Rings",
+      ],
+    },
+    {
+      title: "Pearl Rings",
+      list: [
+        "Freshwater Pearl Rings",
+        "Akoya Pearl Rings",
+        "Tahitian Pearl Rings",
+        "South Sea Pearl Rings",
+      ],
+    },
+    {
+      title: "Rings By shape",
+      list: ["Round Rings", "Oval Rings", "Emerald Rings", "Pear Rings"],
+    },
+    {
+      title: "Rings By style",
+      list: [
+        "Stackable Rings",
+        "Classic Rings",
+        "Vintage Rings",
+        "Side Stone Rings",
+        "Halo Rings",
+        "Solitaire Rings",
+      ],
+    },
+    {
+      title: "Rings By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Rings",
+        "Cocktail Rings",
+        "Three Stone Rings",
+        "Promise Rings",
+        "GIA Certified Rings",
+        "Aniversary Rings",
+      ],
+    },
+  ],
+  Necklaces: [
+    {
+      title: "Natural Gemstone Necklaces",
+      list: [
+        "Blue Sapphire Necklaces",
+        "Ruby Necklaces",
+        "Emerald Necklaces",
+        "Opal Necklaces",
+        "Aquamarine Necklaces",
+        "Amethyst Necklaces",
+        "Garnet Necklaces",
+        "London Blue Topaz Necklaces",
+        "Morganite Necklaces",
+        "Tanzanite Necklaces",
+        "Citrine Necklaces",
+      ],
+    },
+    {
+      title: "Natural Diamond Necklaces",
+      list: ["Diamond Necklaces", "Colored Diamond Necklaces"],
+    },
+    {
+      title: "Lab-Grown Necklaces",
+      list: [
+        "Lab Diamond Necklaces",
+        "Lab Colored Diamond Necklaces",
+        "Lab Emerald Necklaces",
+        "Lab Blue Sapphire Necklaces",
+        "Lab Ruby Necklaces",
+      ],
+    },
+    {
+      title: "Pearl Necklaces",
+      list: [
+        "Freshwater Pearl Necklaces",
+        "Akoya Pearl Necklaces",
+        "Tahitian Pearl Necklaces",
+        "South Sea Pearl Necklaces",
+      ],
+    },
+    {
+      title: "Necklaces By shape",
+      list: [
+        "Round Necklaces",
+        "Oval Necklaces",
+        "Emerald Necklaces",
+        "Pear Necklaces",
+      ],
+    },
+    {
+      title: "Necklaces By style",
+      list: [
+        "Stackable Necklaces",
+        "Classic Necklaces",
+        "Vintage Necklaces",
+        "Side Stone Necklaces",
+        "Halo Necklaces",
+        "Solitaire Necklaces",
+      ],
+    },
+    {
+      title: "Necklaces By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Necklaces",
+        "Cocktail Necklaces",
+        "Three Stone Necklaces",
+        "Promise Necklaces",
+        "GIA Certified Necklaces",
+        "Aniversary Necklaces",
+      ],
+    },
+  ],
+  Earrings: [
+    {
+      title: "Natural Gemstone Earrings",
+      list: [
+        "Blue Sapphire Earrings",
+        "Ruby Earrings",
+        "Emerald Earrings",
+        "Opal Earrings",
+        "Aquamarine Earrings",
+        "Amethyst Earrings",
+        "Garnet Earrings",
+        "London Blue Topaz Earrings",
+        "Morganite Earrings",
+        "Tanzanite Earrings",
+        "Citrine Earrings",
+      ],
+    },
+    {
+      title: "Natural Diamond Earrings",
+      list: ["Diamond Earrings", "Colored Diamond Earrings"],
+    },
+    {
+      title: "Lab-Grown Earrings",
+      list: [
+        "Lab Diamond Earrings",
+        "Lab Colored Diamond Earrings",
+        "Lab Emerald Earrings",
+        "Lab Blue Sapphire Earrings",
+        "Lab Ruby Earrings",
+      ],
+    },
+    {
+      title: "Pearl Earrings",
+      list: [
+        "Freshwater Pearl Earrings",
+        "Akoya Pearl Earrings",
+        "Tahitian Pearl Earrings",
+        "South Sea Pearl Earrings",
+      ],
+    },
+    {
+      title: "Earrings By shape",
+      list: [
+        "Round Earrings",
+        "Oval Earrings",
+        "Emerald Earrings",
+        "Pear Earrings",
+      ],
+    },
+    {
+      title: "Earrings By style",
+      list: [
+        "Stackable Earrings",
+        "Classic Earrings",
+        "Vintage Earrings",
+        "Side Stone Earrings",
+        "Halo Earrings",
+        "Solitaire Earrings",
+      ],
+    },
+    {
+      title: "Earrings By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Earrings",
+        "Cocktail Earrings",
+        "Three Stone Earrings",
+        "Promise Earrings",
+        "GIA Certified Earrings",
+        "Aniversary Earrings",
+      ],
+    },
+  ],
+  "Engagement Rings": [
+    {
+      title: "Natural Gemstone EngageMent Rings",
+      list: [
+        "Blue Sapphire EngageMent Rings",
+        "Ruby EngageMent Rings",
+        "Emerald EngageMent Rings",
+        "Opal EngageMent Rings",
+        "Aquamarine EngageMent Rings",
+        "Amethyst EngageMent Rings",
+        "Garnet EngageMent Rings",
+        "London Blue Topaz EngageMent Rings",
+        "Morganite EngageMent Rings",
+        "Tanzanite EngageMent Rings",
+        "Citrine EngageMent Rings",
+      ],
+    },
+    {
+      title: "Natural Diamond EngageMent Rings",
+      list: ["Diamond EngageMent Rings", "Colored Diamond EngageMent Rings"],
+    },
+    {
+      title: "Lab-Grown EngageMent Rings",
+      list: [
+        "Lab Diamond EngageMent Rings",
+        "Lab Colored Diamond EngageMent Rings",
+        "Lab Emerald EngageMent Rings",
+        "Lab Blue Sapphire EngageMent Rings",
+        "Lab Ruby EngageMent Rings",
+      ],
+    },
+    {
+      title: "Pearl EngageMent Rings",
+      list: [
+        "Freshwater Pearl EngageMent Rings",
+        "Akoya Pearl EngageMent Rings",
+        "Tahitian Pearl EngageMent Rings",
+        "South Sea Pearl EngageMent Rings",
+      ],
+    },
+    {
+      title: "EngageMent Rings By shape",
+      list: [
+        "Round EngageMent Rings",
+        "Oval EngageMent Rings",
+        "Emerald EngageMent Rings",
+        "Pear EngageMent Rings",
+      ],
+    },
+    {
+      title: "EngageMent Rings By style",
+      list: [
+        "Stackable EngageMent Rings",
+        "Classic EngageMent Rings",
+        "Vintage EngageMent Rings",
+        "Side Stone EngageMent Rings",
+        "Halo EngageMent Rings",
+        "Solitaire EngageMent Rings",
+      ],
+    },
+    {
+      title: "EngageMent Rings By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's EngageMent Rings",
+        "Cocktail EngageMent Rings",
+        "Three Stone EngageMent Rings",
+        "Promise EngageMent Rings",
+        "GIA Certified EngageMent Rings",
+        "Aniversary EngageMent Rings",
+      ],
+    },
+  ],
+  "Wedding Rings": [
+    {
+      title: "Natural Gemstone Wedding Rings",
+      list: [
+        "Blue Sapphire Rings",
+        "Ruby Rings",
+        "Emerald Rings",
+        "Opal Rings",
+        "Aquamarine Rings",
+        "Amethyst Rings",
+        "Garnet Rings",
+        "London Blue Topaz Rings",
+        "Morganite Rings",
+        "Tanzanite Rings",
+        "Citrine Rings",
+      ],
+    },
+    {
+      title: "Natural Diamond Rings",
+      list: ["Diamond Rings", "Colored Diamond Rings"],
+    },
+    {
+      title: "Lab-Grown Rings",
+      list: [
+        "Lab Diamond Rings",
+        "Lab Colored Diamond Rings",
+        "Lab Emerald Rings",
+        "Lab Blue Sapphire Rings",
+        "Lab Ruby Rings",
+      ],
+    },
+    {
+      title: "Pearl Rings",
+      list: [
+        "Freshwater Pearl Rings",
+        "Akoya Pearl Rings",
+        "Tahitian Pearl Rings",
+        "South Sea Pearl Rings",
+      ],
+    },
+    {
+      title: "Rings By shape",
+      list: ["Round Rings", "Oval Rings", "Emerald Rings", "Pear Rings"],
+    },
+    {
+      title: "Rings By style",
+      list: [
+        "Stackable Rings",
+        "Classic Rings",
+        "Vintage Rings",
+        "Side Stone Rings",
+        "Halo Rings",
+        "Solitaire Rings",
+      ],
+    },
+    {
+      title: "Rings By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Rings",
+        "Cocktail Rings",
+        "Three Stone Rings",
+        "Promise Rings",
+        "GIA Certified Rings",
+        "Aniversary Rings",
+      ],
+    },
+  ],
+  Bracelets: [
+    {
+      title: "Natural Gemstone Braceltes",
+      list: [
+        "Blue Sapphire Braceltes",
+        "Ruby Braceltes",
+        "Emerald Braceltes",
+        "Opal Braceltes",
+        "Aquamarine Braceltes",
+        "Amethyst Braceltes",
+        "Garnet Braceltes",
+        "London Blue Topaz Braceltes",
+        "Morganite Braceltes",
+        "Tanzanite Braceltes",
+        "Citrine Braceltes",
+      ],
+    },
+    {
+      title: "Natural Diamond Braceltes",
+      list: ["Diamond Braceltes", "Colored Diamond Braceltes"],
+    },
+    {
+      title: "Lab-Grown Braceltes",
+      list: [
+        "Lab Diamond Braceltes",
+        "Lab Colored Diamond Braceltes",
+        "Lab Emerald Braceltes",
+        "Lab Blue Sapphire Braceltes",
+        "Lab Ruby Braceltes",
+      ],
+    },
+    {
+      title: "Pearl Braceltes",
+      list: [
+        "Freshwater Pearl Braceltes",
+        "Akoya Pearl Braceltes",
+        "Tahitian Pearl Braceltes",
+        "South Sea Pearl Braceltes",
+      ],
+    },
+    {
+      title: "Braceltes By shape",
+      list: [
+        "Round Braceltes",
+        "Oval Braceltes",
+        "Emerald Braceltes",
+        "Pear Braceltes",
+      ],
+    },
+    {
+      title: "Braceltes By style",
+      list: [
+        "Stackable Braceltes",
+        "Classic Braceltes",
+        "Vintage Braceltes",
+        "Side Stone Braceltes",
+        "Halo Braceltes",
+        "Solitaire Braceltes",
+      ],
+    },
+    {
+      title: "Braceltes By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Braceltes",
+        "Cocktail Braceltes",
+        "Three Stone Braceltes",
+        "Promise Braceltes",
+        "GIA Certified Braceltes",
+        "Aniversary Braceltes",
+      ],
+    },
+  ],
+  Collections: [
+    {
+      title: "Natural Gemstone Collections",
+      list: [
+        "Blue Sapphire Collections",
+        "Ruby Collections",
+        "Emerald Collections",
+        "Opal Collections",
+        "Aquamarine Collections",
+        "Amethyst Collections",
+        "Garnet Collections",
+        "London Blue Topaz Collections",
+        "Morganite Collections",
+        "Tanzanite Collections",
+        "Citrine Collections",
+      ],
+    },
+    {
+      title: "Natural Diamond Collections",
+      list: ["Diamond Collections", "Colored Diamond Collections"],
+    },
+    {
+      title: "Lab-Grown Collections",
+      list: [
+        "Lab Diamond Collections",
+        "Lab Colored Diamond Collections",
+        "Lab Emerald Collections",
+        "Lab Blue Sapphire Collections",
+        "Lab Ruby Collections",
+      ],
+    },
+    {
+      title: "Pearl Collections",
+      list: [
+        "Freshwater Pearl Collections",
+        "Akoya Pearl Collections",
+        "Tahitian Pearl Collections",
+        "South Sea Pearl Collections",
+      ],
+    },
+    {
+      title: "Collections By shape",
+      list: [
+        "Round Collections",
+        "Oval Collections",
+        "Emerald Collections",
+        "Pear Collections",
+      ],
+    },
+    {
+      title: "Collections By style",
+      list: [
+        "Stackable Collections",
+        "Classic Collections",
+        "Vintage Collections",
+        "Side Stone Collections",
+        "Halo Collections",
+        "Solitaire Collections",
+      ],
+    },
+    {
+      title: "Collections By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Collections",
+        "Cocktail Collections",
+        "Three Stone Collections",
+        "Promise Collections",
+        "GIA Certified Collections",
+        "Aniversary Collections",
+      ],
+    },
+  ],
+  Gifts: [
+    {
+      title: "Gifts",
+      list: [
+        "Blue Sapphire Gifts",
+        "Ruby Gifts",
+        "Emerald Gifts",
+        "Opal Gifts",
+        "Aquamarine Gifts",
+        "Amethyst Gifts",
+        "Garnet Gifts",
+        "London Blue Topaz Gifts",
+        "Morganite Gifts",
+        "Tanzanite Gifts",
+        "Citrine Gifts",
+      ],
+    },
+    {
+      title: "Natural Diamond Gifts",
+      list: ["Diamond Gifts", "Colored Diamond Gifts"],
+    },
+    {
+      title: "Lab-Grown Gifts",
+      list: [
+        "Lab Diamond Gifts",
+        "Lab Colored Diamond Gifts",
+        "Lab Emerald Gifts",
+        "Lab Blue Sapphire Gifts",
+        "Lab Ruby Gifts",
+      ],
+    },
+    {
+      title: "Pearl Gifts",
+      list: [
+        "Freshwater Pearl Gifts",
+        "Akoya Pearl Gifts",
+        "Tahitian Pearl Gifts",
+        "South Sea Pearl Gifts",
+      ],
+    },
+    {
+      title: "Gifts By shape",
+      list: ["Round Gifts", "Oval Gifts", "Emerald Gifts", "Pear Gifts"],
+    },
+    {
+      title: "Gifts By style",
+      list: [
+        "Stackable Gifts",
+        "Classic Gifts",
+        "Vintage Gifts",
+        "Side Stone Gifts",
+        "Halo Gifts",
+        "Solitaire Gifts",
+      ],
+    },
+    {
+      title: "Gifts By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Gifts",
+        "Cocktail Gifts",
+        "Three Stone Gifts",
+        "Promise Gifts",
+        "GIA Certified Gifts",
+        "Aniversary Gifts",
+      ],
+    },
+  ],
+  Edit: [
+    {
+      title: "Edit yours",
+      list: [
+        "Blue Sapphire Rings",
+        "Ruby Rings",
+        "Emerald Rings",
+        "Opal Rings",
+        "Aquamarine Rings",
+        "Amethyst Rings",
+        "Garnet Rings",
+        "London Blue Topaz Rings",
+        "Morganite Rings",
+        "Tanzanite Rings",
+        "Citrine Rings",
+      ],
+    },
+    {
+      title: "Natural Diamond Rings",
+      list: ["Diamond Rings", "Colored Diamond Rings"],
+    },
+    {
+      title: "Lab-Grown Rings",
+      list: [
+        "Lab Diamond Rings",
+        "Lab Colored Diamond Rings",
+        "Lab Emerald Rings",
+        "Lab Blue Sapphire Rings",
+        "Lab Ruby Rings",
+      ],
+    },
+    {
+      title: "Pearl Rings",
+      list: [
+        "Freshwater Pearl Rings",
+        "Akoya Pearl Rings",
+        "Tahitian Pearl Rings",
+        "South Sea Pearl Rings",
+      ],
+    },
+    {
+      title: "Rings By shape",
+      list: ["Round Rings", "Oval Rings", "Emerald Rings", "Pear Rings"],
+    },
+    {
+      title: "Rings By style",
+      list: [
+        "Stackable Rings",
+        "Classic Rings",
+        "Vintage Rings",
+        "Side Stone Rings",
+        "Halo Rings",
+        "Solitaire Rings",
+      ],
+    },
+    {
+      title: "Rings By Birthstone",
+      list: ["Tanzanite - December", "Garnet - January", "Amethyst - February"],
+    },
+    {
+      title: "Fearured",
+      list: [
+        "All-New Men's Rings",
+        "Cocktail Rings",
+        "Three Stone Rings",
+        "Promise Rings",
+        "GIA Certified Rings",
+        "Aniversary Rings",
+      ],
+    },
+  ],
+};
